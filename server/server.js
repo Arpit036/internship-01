05fbad98907d623023dba8dbb09ee86924eedc18
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-11-28 17:39:43 +0530

    server setup

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const PORT = 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+let items = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' }
+];
+
+
+app.get('/api/items', (req, res) => {
+    res.json(items);
+});
+
+app.post('/api/items', (req, res) => {
+    const newItem = {
+        id: items.length + 1,
+        name: req.body.name
+    };
+    items.push(newItem);
+    res.status(201).json(newItem);
+});
+
+app.delete('/api/items/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    items = items.filter(item => item.id !== itemId);
+    res.status({ message: 'Item deleted' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
